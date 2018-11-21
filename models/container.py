98fbf8a2d7987deb1294bd059ea1727c3e3390ed
Viktor Gustafsson <vgustafsson@planview.com>
2018-11-21 13:47:36 +0100

    minor styles

--- models/container.py
+++ models/container.py
@@ -112,7 +112,7 @@ class Container(object):
                     <style type="text/css">
                     body {
                         font-size: 16px;
-                        font-family: sans-serif;
+                        font-family: "PromixaNovaRegular", "AvenirRegular", Arial, Helvetica, sans-serif;
                         margin: 0;
                         padding: 0;
                     }
@@ -129,14 +129,21 @@ class Container(object):
                     div.breadcrumbs {
                         margin: 0px;
                         padding: 20px;
-                        border-bottom: 1px solid #333;
-                        background-color: #dadada;
+                        border-bottom: 2px solid #ddd;
+                        background-color: #f5f5f5;
                     }
                     ul li {
                         margin-bottom: 10px;
                     }
                     div.content {
-                        padding: 10px 20px 0 20px;
+                        padding: 10px 20px 0;
+                    }
+                    h3 {
+                        display: flex;
+                        height: 1em;
+                    }
+                    h3 svg {
+                        height: 1em;
                     }
                     </style>
                </head>
@@ -165,7 +172,7 @@ class Container(object):
             return containers_html
 
         return """
-               <h2>Folders:</h2>
+               <h3><svg aria-hidden="true" data-prefix="far" data-icon="folder" class="svg-inline--fa fa-folder fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M464 128H272l-54.63-54.63c-6-6-14.14-9.37-22.63-9.37H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V176c0-26.51-21.49-48-48-48zm0 272H48V112h140.12l54.63 54.63c6 6 14.14 9.37 22.63 9.37H464v224z"></path></svg>&nbsp;Folders</h3>
                <ul>
                %s
                </ul>
@@ -186,7 +193,7 @@ class Container(object):
             return documents_html
 
         return """
-               <h2>Documents:</h2>
+                   <h3><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" data-prefix="far" data-icon="file" class="svg-inline--fa fa-file fa-w-12" role="img" viewBox="0 0 384 512"><path fill="currentColor" d="M369.9 97.9L286 14C277 5 264.8-.1 252.1-.1H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V131.9c0-12.7-5.1-25-14.1-34zM332.1 128H256V51.9l76.1 76.1zM48 464V48h160v104c0 13.3 10.7 24 24 24h104v288H48z"/></svg>&nbsp;Documents</h3>
                <ul>
                %s
                </ul>
@@ -219,3 +226,4 @@ class Container(object):
             fp.write(self.html_container_content(containers))
             fp.write(self.html_document_content(documents))
             fp.write(self.html_footer)
+
--- models/workspace.py
+++ models/workspace.py
@@ -50,10 +50,10 @@ class Workspace(object):
                 <title>%(workspace_name)s</title>
                 <style type="text/css">
                 body {
-                    font-size: 16px;
-                    font-family: sans-serif;
-                    margin: 0;
-                    padding: 0;
+                        font-size: 16px;
+                        font-family: "PromixaNovaRegular", "AvenirRegular", Arial, Helvetica, sans-serif;
+                        margin: 0;
+                        padding: 0;
                 }
                 a {
                     color: #559955;
@@ -68,8 +68,8 @@ class Workspace(object):
                 div.breadcrumbs {
                     margin: 0px;
                     padding: 20px;
-                    border-bottom: 1px solid #333;
-                    background-color: #dadada;
+                    border-bottom: 2px solid #ddd;
+                    background-color: #f5f5f5;
                 }
                 ul li {
                     margin-bottom: 10px;
@@ -77,6 +77,13 @@ class Workspace(object):
                 div.content {
                     padding: 10px 20px 0 20px;
                 }
+                 h3 {
+                        display: flex;
+                        height: 1em;
+                    }
+                    h3 svg {
+                        height: 1em;
+                    }
             </style>
             </head>
             <body>
@@ -102,7 +109,7 @@ class Workspace(object):
             return containers_html
 
         return """
-            <h2>Folders:</h2>
+            <h3><svg aria-hidden="true" data-prefix="far" data-icon="folder" class="svg-inline--fa fa-folder fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M464 128H272l-54.63-54.63c-6-6-14.14-9.37-22.63-9.37H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V176c0-26.51-21.49-48-48-48zm0 272H48V112h140.12l54.63 54.63c6 6 14.14 9.37 22.63 9.37H464v224z"></path></svg>&nbsp;Folders</h3>
             <ul>
             %s
             </ul>
@@ -123,7 +130,7 @@ class Workspace(object):
             return documents_html
 
         return """
-                   <h2>Documents:</h2>
+                   <h3><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" data-prefix="far" data-icon="file" class="svg-inline--fa fa-file fa-w-12" role="img" viewBox="0 0 384 512"><path fill="currentColor" d="M369.9 97.9L286 14C277 5 264.8-.1 252.1-.1H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48V131.9c0-12.7-5.1-25-14.1-34zM332.1 128H256V51.9l76.1 76.1zM48 464V48h160v104c0 13.3 10.7 24 24 24h104v288H48z"/></svg>&nbsp;Documents</h3>
                    <ul>
                    %s
                    </ul>
